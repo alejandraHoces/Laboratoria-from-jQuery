e289e663864b107ec8bfb479f82868339e6ec5fa
Alejandra <hocesalejandra@gmail.com>
2017-12-22 16:44:30 -0500

    clase guiada

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-  $('#submit').attr("disabled", true);
+  var $btn = $('#submit');
+  $btn.attr("disabled", true);
+
+  var inputUser = $('#username');
+  $(inputUser).keydown(function () {
+    if(inputUser.val().length >= 3){
+      $('p:first-of-type span').hide();
+    } else {
+      $('p:first-of-type span').show();
+    }
+  })
+
+  var passwordUser = $('#password');
+  $(passwordUser).keydown(function () {
+    if(passwordUser.val().length >= 6){
+      $('p:nth-of-type(2) span').hide();
+    } else {
+      $('p:nth-of-type(2) span').show();
+    }
+  })
+
+  var passConfirm = $('#confirm_password');
+  $(passConfirm).keydown(function () {
+    if(passConfirm.val().length >= 6){
+      $('p:nth-of-type(3) span').hide();
+    } else if(passwordUser.val === passConfirm.val) {
+      $btn.attr("disabled", false);
+    }else {
+      $('p:nth-of-type(3) span').show();
+    }
+  })
 })
